5c1e3de6ab0970a2d03b4a366358adbfde3f6aad
Alaa551 <alaahassan2019th@gmail.com>
2024-08-06 05:42:23 +0300

    first commit

---
+++ app/src/main/java/com/example/cityexplorer/data/local/model/City.kt
@@ -0,0 +1,9 @@
+package com.example.cityexplorer.data.local.model
+
+
+data class City(
+    val _id: Int,
+    val coord: Coord,
+    val country: String,
+    val name: String
+)
---
+++ app/src/main/java/com/example/cityexplorer/data/local/model/Coord.kt
@@ -0,0 +1,6 @@
+package com.example.cityexplorer.data.local.model
+
+data class Coord(
+    val lat: Double,
+    val lon: Double
+)
